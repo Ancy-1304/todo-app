76345ad12d40a89f7b232970fe87802dd15afae9
Ancy-1304 <ancyyl134@gmail.com>
2025-04-24 08:30:18 +0530

    Add todo component and fix CSS

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:'/todo-app/',
+  base: '/todo-app/', // This MUST match your repo name exactly
   plugins: [react()],
 })
